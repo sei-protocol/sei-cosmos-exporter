d0515d6937cbdb5ecb7ea2a7ea58671b9c644f95
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-03-29 14:13:03 -0400

    output

--- install-sei-cosmos-exporter.sh
+++ install-sei-cosmos-exporter.sh
@@ -5,7 +5,7 @@ BENCH_PREFIX=sei
 
 echo -e "\e[1m\e[32m1. Installing cosmos-exporter... \e[0m" && sleep 1
 # install cosmos-exporter
-go build -o build/sei-cosmos-exporter
+go build -o build/sei-cosmos-exporter ./
 sudo mv ./sei-cosmos-exporter /usr/bin
 
 sudo useradd -rs /bin/false cosmos_exporter
